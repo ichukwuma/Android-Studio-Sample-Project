922d4d0a84ac51b4ddfe2673fd8c8b3f27af3a68
ij <ijeomachukwuma@my.unt.edu>
2024-04-30 13:10:43 -0500

    Completed Project #3

--- app/src/main/java/com/example/project_3/MainActivity.java
+++ app/src/main/java/com/example/project_3/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+//adding this for event handling later
 public class MainActivity extends AppCompatActivity {
 
     private ImageView images;
