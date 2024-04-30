d3876efee77914d6f785c21d81f9e7bceaca5526
ij <ijeomachukwuma@my.unt.edu>
2024-04-30 13:01:18 -0500

    Completed Project #3

---
+++ app/src/main/java/com/example/project_3/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.project_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        images = findViewById(R.id.images);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images.setImageResource(R.drawable.winning);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images.setImageResource(R.drawable.cobrakai);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images.setImageResource(R.drawable.s1);
+            }
+        });
+    }
+}
